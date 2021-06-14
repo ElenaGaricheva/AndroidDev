94d4a4eab1c69d296a05608518fb81e2c928d6ab
ElenaGaricheva <elenagaricheva95@gmail.com>
2021-06-14 21:59:10 +0300

    Homework.Lesson 6 fix

--- src/main/java/ru/geekbrains/homework/Lesson6/ActivityType.java
+++ src/main/java/ru/geekbrains/homework/Lesson6/ActivityType.java
@@ -1,18 +1,6 @@
 package ru.geekbrains.homework.Lesson6;
 
 public enum ActivityType {
-    SWIM ("swim"),
-    RUN ("run"),
-    RAN ("ran"),
-    SWAM ("swam");
-
-    private final String activityType;
-
-    ActivityType(String activityType) {
-        this.activityType = activityType;
-    }
-
-    public String getType() {
-        return activityType;
-    }
+    SWIM,
+    RUN
 }
