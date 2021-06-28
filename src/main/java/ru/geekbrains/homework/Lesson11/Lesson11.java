db341b2e45dc697fd848c4326b884a5fa66748b2
ElenaGaricheva <elenagaricheva95@gmail.com>
2021-06-28 18:07:47 +0300

    Homework.Lesson 11

--- src/main/java/ru/geekbrains/homework/Lesson11/Lesson11.java
+++ src/main/java/ru/geekbrains/homework/Lesson11/Lesson11.java
@@ -1,4 +1,27 @@
 package ru.geekbrains.homework.Lesson11;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Lesson11 {
+    public static void main(String[] args) {
+        String[] wordsArray = {
+                "one", "two", "three",
+                "four", "five", "six",
+                "six", "six", "six", "two",
+                "two", "two", "two", "five",
+                "five", "five"
+        };
+        System.out.println(checkUniqueWords(wordsArray));
+    }
+
+    public static ArrayList<String> checkUniqueWords(String[] wordsArr){
+        ArrayList<String> foundWords = new ArrayList<>();
+        for (String word: wordsArr) {
+            if (!foundWords.contains(word)) {
+                foundWords.add(word);
+            }
+        }
+        return foundWords;
+    }
 }
