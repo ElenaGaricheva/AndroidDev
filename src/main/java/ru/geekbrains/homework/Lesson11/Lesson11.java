ef7a366bdf119ceaaed875653a71f5a7fd41d41b
ElenaGaricheva <elenagaricheva95@gmail.com>
2021-06-28 21:50:51 +0300

    Homework.Lesson 11

--- src/main/java/ru/geekbrains/homework/Lesson11/Lesson11.java
+++ src/main/java/ru/geekbrains/homework/Lesson11/Lesson11.java
@@ -13,11 +13,18 @@ public class Lesson11 {
                 "five", "five"
         };
         System.out.println(checkUniqueWords(wordsArray));
+
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Test_1", "89999999999");
+        phoneBook.add("Test_2", "87777777777");
+        phoneBook.add("Test_1", "86666666666");
+        phoneBook.get("Test_1");
+        phoneBook.get("Test_2");
     }
 
-    public static ArrayList<String> checkUniqueWords(String[] wordsArr){
+    public static ArrayList<String> checkUniqueWords(String[] wordsArr) {
         ArrayList<String> foundWords = new ArrayList<>();
-        for (String word: wordsArr) {
+        for (String word : wordsArr) {
             if (!foundWords.contains(word)) {
                 foundWords.add(word);
             }
